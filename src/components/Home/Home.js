c444c6721178a5e7199dca9abbec72c90f5f1d5e
tommitoan <htm1995@gmail.com>
2024-06-25 23:57:12 +0700

    update

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -16,7 +16,7 @@ function Home() {
               <h1 style={{ paddingBottom: 15 }} className="heading">
                 Hi There!{" "}
                 <span className="wave" role="img" aria-labelledby="wave">
-                  👋🏻
+                  👋
                 </span>
               </h1>
 
